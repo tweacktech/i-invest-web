ceb93cd11e1cdfa1de11ca952d10be2be5a40ec7
daridaniel <dari.daniel@moneta.ng>
2026-06-11 19:50:54 +0100

    added google adsent

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,14 +38,14 @@ export default function RootLayout({
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-      </head>
-      <body className="min-h-full flex flex-col">
-        <Script
+        <link rel="manifest" href="/site.webmanifest" /><Script
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5937965903229328"
           strategy="afterInteractive"
           crossOrigin="anonymous"
         />
+      </head>
+      <body className="min-h-full flex flex-col">
+        
         <Providers>
           <CurrencyProvider>
             {children}
